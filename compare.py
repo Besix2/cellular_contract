27676ad95a19f88550cb2dd93c4435cca6cd5726
Besix2 <myusernmaethis@outlook.de>
2023-12-26 18:08:11 +0100

    rewrote logic and added manual checks

--- compare.py
+++ compare.py
@@ -11,7 +11,8 @@ import json
 
 
 def main():
-    html_cache()
+    for i in html_cache():
+        print(i)
 
 #loads the json directory from the file
 def read_dictionary_from_file():
@@ -40,8 +41,8 @@ def check_price_cache(phone_name):
 def get_price(phone_name):
     options = webdriver.ChromeOptions()
     options.add_argument('--headless')
-
-    base_url = f"https://www.zoxs.de/ankauf_suche.html?q={phone_name}"
+    html_phone_name = phone_name.replace(" ", "%20")
+    base_url = f"https://www.zoxs.de/ankauf_suche.html?q={html_phone_name}"
     driver = webdriver.Chrome(options=options)
     driver.get(base_url)
     #handling cookie button
@@ -57,16 +58,20 @@ def get_price(phone_name):
         extracted_name = item.find(class_="bottom").text
         phone_name_split = [*phone_name]
         extracted_name_split = [*extracted_name]
+        pass
         #check if result matches the phone name
         if all(letter in extracted_name_split for letter in phone_name_split):
             link_element = item.find(class_="text-dark nav-link p-0 rounded offer-tile-klickarea")
             phone_sell_url = "https://www.zoxs.de/" + link_element["href"]
+            print(phone_sell_url)
             driver.get(phone_sell_url)
             soup2 = BeautifulSoup(driver.page_source, "html.parser")
             try:
                 price_element = soup2.find(id="productPrice", class_="h2 mb-0 text-grey-5 font-size-md")
                 price_element2 = price_element.find('input', {'name': 'gesamt'})
                 value = price_element2["value"]
+                if value == None:
+                    pass
                 print(value)
                 print(phone_name)
                 price = float(value.replace(' EUR', '').replace(",","."))
@@ -77,20 +82,27 @@ def get_price(phone_name):
                 print(error)
                 return 0
             #manually adding prices that couldnt be resolved by the code because the phone name is misleading
-        else:
-            print("\n")
-            print(phone_name)
-            print("Link: " + f"https://www.zoxs.de/ankauf_suche.html?q={phone_name}")
-            manual_price = float(input("Manuelle Preiseingabe: ").replace(",","."))
-            return manual_price
-            pass
-      
+
+    print("\n")
+    print(phone_name)
+    print("Link: " + f"https://www.zoxs.de/ankauf_suche.html?q={html_phone_name}")
+    manual_price = float(input("Manuelle Preiseingabe: ").replace(",","."))
+    return manual_price
+    pass
+def get_html(url):   
+    options = webdriver.ChromeOptions()
+    options.add_argument('--headless')
+    driver = webdriver.Chrome(options=options)
+    driver.get(url)
+    #wait 6 seconds for the direction to the deal site
+    time.sleep(6)
+    return driver.page_source
 def scrape_selenium():
     options = webdriver.ChromeOptions()
     options.add_argument('--headless')
 
     # put url here
-    url = "https://www.handyhase.de/handy-mit-vertrag/#data-volume=5&download-speed=99999&monthly-costs=99999&onetimecosts=99999&device-rom=99999&phone-options=99999&contract-period=99999&cancelable-automatic-data-renewal=99999&manufacturerId=99999&classification=99999&providerId=99999&shopId=99999&sort=4&young=1&data5g=0&combined=1&esim=0&multicard=0&landline-number=0&device5g=0&cellular-network=1%2C3%2C5"
+    url = "https://www.handyhase.de/samsung/galaxy-s23/#data-volume=5&download-speed=99999&monthly-costs=99999&onetimecosts=99999&device-rom=99999&phone-options=99999&contract-period=99999&cancelable-automatic-data-renewal=99999&classification=99999&providerId=99999&shopId=99999&sort=4&young=0&data5g=0&combined=0&esim=0&multicard=0&landline-number=0&device5g=0&cellular-network=1%2C3%2C5&deviceId=1056"
     # put url here
     driver = webdriver.Chrome(options=options)
     driver.get(url)
@@ -124,33 +136,39 @@ def scrape_selenium():
     return driver.page_source
 
 def html_cache(): #check if html is cached if not cache it
+    skip_element = []
     if os.path.getsize("cache.html") < 1:
         html = scrape_selenium()
         with open("cache.html", "w", encoding="utf-8") as file:
             file.write(html)
+        return soup_process(html, skip_element)
     else:
         with open("cache.html", "r", encoding="utf-8") as file:
             html = file.read()
-    soup_process(html)
+    return soup_process(html, skip_element)
         
-def soup_process(page_source): #function to compare pricess
-    last_price = 999
+def soup_process(page_source, skip_element): #function to compare pricess
+    last_price = 9999
     last_data = 0
     last_phone_name = "test"
     soup = BeautifulSoup(page_source, "html.parser")
     grid = soup.find(id="collapse-compare", attrs={"data-type": "phone-and-tariff"})
     elements = grid.contents
+    elements = [element for element in elements if element != "\n"]
+    if skip_element != None:
+            pass
     for element in elements:
-        if element == "\n":
-            continue
+        contract_link = element.find("a", class_="compare-item__cta btn green")
+        contract_url = contract_link["href"]
+        if len(skip_element) > 0:
+            if contract_url in skip_element:
+                continue
+        
         price_wrapper = element.find("div", class_="compare-item__prices-wrapper pricingdetail")
-        wrapper_parent = price_wrapper.find("div", class_="compare-item__prices")
-        price_wrapper_child = wrapper_parent.find("div", class_="compare-item__regular-prices")
-        price_wrapper_child_list = price_wrapper_child.find_all("div", class_="compare-item__regular-price-wrapper")
-        price_wrapper_child1 = price_wrapper_child_list[0]
-        price_wrapper_child2 = price_wrapper_child_list[1]
-        monthly_price = price_wrapper_child1.find("div", class_="compare-item__regular-price").text
-        onetime_price = price_wrapper_child2.find("div", class_="compare-item__regular-price").text
+        wrapper_child = price_wrapper.find("div", class_="compare-item__regular-average-wrapper")
+        average_montly_price = wrapper_child.find("div", class_="compare-item__regular-average-price").text
+        average_montly_price = numbers = re.findall(r'\d[\d,.]*', average_montly_price)
+        average_montly_price = float(numbers[0].replace(',', '.')) if numbers else None
         head_wrapper = element.find("div", class_="compare-item__head-wrapper")
         meta_wrapper = head_wrapper.find("div", class_="compare-item__meta-wrapper")
         phone_name = meta_wrapper.find("div", class_="compare-item__meta-device").text
@@ -164,26 +182,27 @@ def soup_process(page_source): #function to compare pricess
             cellular_data = float(cellular_data_match.group(1)) if cellular_data_match else 0
         phone_name = re.sub(r'\([^)]*\)', '', phone_name)
         phone_name = phone_name.strip()
-        Final_price = check_price_cache(phone_name)
         # getting all the data
-        # with open("prices.json", "r") as prices:
-        #     prices_list = json.loads(prices.read())
-        # phone_price = prices_list.get(phone_name, "0")
-        # monthly_price = monthly_price.replace(".", "").replace("€", "").replace(",", ".").strip()
-        # onetime_price = onetime_price.replace(".", "").replace("€", "").replace(",", ".").strip()
-        # phone_price = phone_price.replace("€", "").replace(".", "").replace(",", ".").strip()
-        # Final_price = (24 * float(monthly_price) + float(onetime_price)) - float(phone_price)
-        # Final_price = Final_price / 24
-        #cleaning the data
-        if 4 > Final_price and cellular_data > last_data:
+        phone_price = check_price_cache(phone_name)
+        Final_price = (average_montly_price * 24 - float(phone_price)) / 24
+        if Final_price < last_price:
+        # basicly a filter
+        # if Final_price < 4 and cellular_data > last_data:
             last_data = cellular_data
             last_price = Final_price
+            last_contract_url = contract_url
             last_phone_name = phone_name
-            contract_link = element.find("a", class_="compare-item__cta btn green")
-            # basicly a filter
-    print(last_price)
-    print(last_phone_name)
-    print(contract_link["href"])
-    # print the results
+    
+    if "Refurbished" in str(get_html(last_contract_url)):
+        print("\n" + last_contract_url)
+        answer = input("Refurbished?(ja oder nein): ").lower()
+        if answer == "ja":
+            #recursive call to filter out refurbished devices
+            skip_element.append(last_contract_url)
+            return soup_process(page_source, skip_element) 
+    
+    return [last_price,last_phone_name,last_contract_url]
+
+    
 
 main()
